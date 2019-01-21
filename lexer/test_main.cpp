78b64c0ec414ba1a5daf2af0a4deadcf5dcd502e
Ivan Folbort <petrarce@gmail.com>
2019-01-21 19:16:13 +0100

    fix test_main for lexer

--- lexer/test_main.cpp
+++ lexer/test_main.cpp
@@ -2,14 +2,21 @@
 #include <types.hpp>
 #include <lexer.hpp>
 #include <lexer_regexps.hpp>
+#include "regexp_links.bin.hpp"
 
 int main(int argc, char** argv)
 {
+	assert(argc == 2);
+	string inp = argv[1];
 
-	lexer new_lexer(regexp_vec, regexp_analysis);
-	string inp_str = argv[1];
+	lexer lexer_linked(regexp_vec, regexp_analysis);
+	lexer_linked.prod_nfa.nfa_bt_run(inp, la_str, vector<uint32_t>());
 
-	new_lexer.prod_nfa.nfa_bt_run(inp_str, la_str, vector<uint32_t>());
+	vector<uint32_t> accepting_states = lexer_linked.prod_nfa.get_accepting();
+	for(uint32_t id : accepting_states){
+		printf("%d, ", id);
+	}
+	printf("\n");
 
 	return 0;
 }
