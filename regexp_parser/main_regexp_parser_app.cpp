75ed2b06765f30c3ad00b4c5805234e8adeab9b8
Ivan Folbort <petrarce@gmail.com>
2019-01-27 23:37:22 +0100

    add regexp application which prints all generated links and accepting states between regexp nfa

---
+++ regexp_parser/main_regexp_parser_app.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <lexer_regexps.hpp>
+#include <regexp_parser.hpp>
+
+using namespace std;
+
+int main(){
+
+	nfa regexp_nfa;
+	int i;
+
+	//parse all regexps and convert them to nfa
+	for(i = 0; i < regexp_vec.size(); i++){
+		nfa tmp_nfa;
+		int status = regex2nfa(regexp_vec[i], regexp_analysis[i], tmp_nfa);
+		assert(status == 0);
+		regexp_nfa = nfa::nfa_convert_union(regexp_nfa, tmp_nfa);
+	}
+
+	string links = regexp_nfa.get_links();
+	auto accepting = regexp_nfa.get_accepting();
+
+	printf("#include <vector>");
+	printf("const vector regexp_links = {\n\t", links[i]);
+	for(i = 0; i < links.size()-1; i++){
+		if(!(i%8) && i != 0){
+			printf("0x%x,\n\t", links[i]);
+			continue;
+		}
+		printf("0x%x, ", links[i]);
+	}
+	printf("0x%x\n};\n", links[i]);
+
+	for(uint32_t state : accepting){
+		printf("%d ", state);
+	}
+	printf("\n");
+}
