d215d0513fa95a6ef393097988a278ab37fcad0c
Ivan Folbort <petrarce@gmail.com>
2019-02-06 23:42:59 +0100

    add progress bar into regexps converter

--- regexp_parser/main_regexp_parser_app.cpp
+++ regexp_parser/main_regexp_parser_app.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
 #include <lexer_regexps.hpp>
+#include <string>
 #include "regexp_parser.hpp"
 
 using namespace std;
+
+inline void update_progress_bar(uint32_t status, uint32_t max){
+	string bar;
+	uint32_t prc = (status*100)/max;
+
+	bar += to_string(prc) + "% ";
+	for(int i = 0; i < prc; i++){
+		bar.push_back('#');
+	}
+	bar += "\r";
+	fprintf(stderr ,"%s", bar.data());
+	cout.flush();
+}
 
 int main(){
 
 	nfa regexp_nfa;
 	int i;
+	string status_str;
+	uint32_t status_prc = 0;
 
 	//parse all regexps and convert them to nfa
 	for(i = 0; i < regexp_vec.size(); i++){
 		nfa tmp_nfa;
+		update_progress_bar(i, regexp_vec.size());
 		int status = regex2nfa(regexp_vec[i], regexp_analysis[i], tmp_nfa);
 		assert(status == 0);
 		regexp_nfa = nfa::nfa_convert_union(regexp_nfa, tmp_nfa);
 	}
+	update_progress_bar(1,1);
+	fprintf(stderr, "\r\n");
 
 	string links = regexp_nfa.get_links();
 	auto accepting = regexp_nfa.get_accepting();
